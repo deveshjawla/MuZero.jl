6e7ce7e9eacbafc4993d0675c509de46cb454929
deveshjawla <devesh.jawla@gmail.com>
2021-04-14 13:09:17 +0200

    Basic MinMax module without struct

--- src/utilities/minmaxstats.jl
+++ src/utilities/minmaxstats.jl
@@ -2,3 +2,31 @@
 ###### Holds the Min-Max values of the tree
 ######
 
+module MinMaxStats
+    export tree_min,tree_max,update,normalize
+
+    """
+    Simply brings variables('tree_min' and 'tree_max') and functions('update' and 'normalize')
+    to the scope.
+    'tree_min,tree_max=MinMaxStats.update(tree_min,tree_max,value::Float64)'
+    'value=MinMaxStats.normalize(tree_min,tree_max,value::Float64)'
+    """
+
+    tree_min=Inf
+    tree_max=-Inf
+
+    function update(tree_min::Float64,tree_max::Float64,value::Float64)::Tuple{Float64, Float64}
+        min=minimum([tree_min,value])
+        max=maximum([tree_max,value])
+        return min, max
+    end
+    
+    function normalize(tree_min::Float64,tree_max::Float64,value::Float64)::Float64
+        if tree_max > tree_min
+            return (value - tree_min)/(tree_max - tree_min)
+        else
+            return value
+        end
+    end
+
+end
--- worklog.md
+++ worklog.md
@@ -2,3 +2,7 @@
 1. Instead of using the readme.md file to keep track of what we are currently working on and what has been done, we use this file.
 2. We can also use very few resources like using a miniscule network and playing only a few games per iteration, this will enable us to run and test our code on our machines.
 3. Also look we should follow the Julia [style guide](https://docs.julialang.org/en/v1/manual/style-guide/) to name varibales and modules etc.
+
+# 2020-04-13
+1. Writing MinMaxstats.jl module
+2. Made repo Public
